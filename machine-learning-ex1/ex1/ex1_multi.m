171eb2e56e82bbbf166d885b4129f58a6013c616
Alex Jenkins <alexanderjenkins96@outlook.com>
2020-02-12 13:33:21 +0000

    Implemented normal equation.
    Added a function that calculates the parameters (theta) using the normal
    equation

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -60,13 +60,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -82,18 +82,34 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha1 = 0.01;
+alpha2 = 0.003;
+alpha3 = 0.001;
+alpha4 = 0.0003;
+num_iters = 1000;
 
-% Init Theta and Run Gradient Descent 
-theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+% Init Theta and Run Gradient Descent
+theta1 = zeros(3, 1);
+theta2 = zeros(3, 1);
+theta3 = zeros(3, 1);
+theta4 = zeros(3, 1);
+[theta1, J1] = gradientDescentMulti(X, y, theta1, alpha1, num_iters);
+[theta2, J2] = gradientDescentMulti(X, y, theta2, alpha2, num_iters);
+[theta3, J3] = gradientDescentMulti(X, y, theta3, alpha3, num_iters);
+[theta4, J4] = gradientDescentMulti(X, y, theta4, alpha4, num_iters);
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+plot(1:50, J1(1:50), 'b');
+hold on;
+plot(1:50, J2(1:50), 'r');
+plot(1:50, J3(1:50), 'k');
+plot(1:50, J4(1:50), 'g');
+
 xlabel('Number of iterations');
 ylabel('Cost J');
+
+J = J1; theta = theta1;
 
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
@@ -105,6 +121,10 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
+x_test = [1650; 3];
+x_test_norm = (x_test - mu') ./ sigma';
+x_test_norm = [1; x_test_norm];
+price = theta' * x_test_norm;
 
 
 % ============================================================
@@ -120,12 +140,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -150,10 +170,10 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+x_test = [1650; 3];
+price = theta' * x_test_norm;
 
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using normal equations):\n $%f\n'], price);
-
