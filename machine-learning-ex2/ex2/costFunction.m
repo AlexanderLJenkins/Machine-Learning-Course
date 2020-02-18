5f664bdc22af03bed9bcd21b4deb27c165998dbc
Alex Jenkins <alexanderjenkins96@outlook.com>
2020-02-18 13:02:07 +0000

    Added function to compute the cost for logistic regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,11 +20,8 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
+hyp = sigmoid(theta'*X');
+J = sum((1/m) * X'*(hyp'-y))
 
 
 % =============================================================
